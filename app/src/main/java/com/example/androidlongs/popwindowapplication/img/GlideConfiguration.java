20411b7425fee47010d635f84a2157a7b6d1d43e
zhaolong <928343994@qq.com>
2016-12-01 22:17:53 +0800

    Glide 图片加载框架 默认常用工具类完成
    清单文件中加入Glide配制
    
    默认加载图片

---
+++ app/src/main/java/com/example/androidlongs/popwindowapplication/img/GlideConfiguration.java
@@ -0,0 +1,106 @@
+package com.example.androidlongs.popwindowapplication.img;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
+import com.bumptech.glide.module.GlideModule;
+
+/**
+ * Created by androidlongs on 16/12/1.
+ * 站在顶峰，看世界
+ * 落在谷底，思人生
+ */
+
+public class GlideConfiguration implements GlideModule {
+
+    /**
+     * 用来在Glide单例创建之前应用所有的选项配置，
+     * 该方法每次实现只会被调用一次。
+     * 通过GlideBuilder(Glide的创造者)我们可以对Glide进行各种配置
+     */
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        //配制加载图片的格式
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+        /**
+         * MemoryCache和BitmapPool的默认大小由MemorySizeCalculator类决定，
+         * MemorySizeCalculator会根据给定屏幕大小可用内存算出合适的缓存大小，这也是推荐的缓存大小
+         */
+        MemorySizeCalculator calculator = new MemorySizeCalculator(context);
+
+        int defaultMemoryCacheSize = calculator.getMemoryCacheSize();
+        int defaultBitmapPoolSize = calculator.getBitmapPoolSize();
+
+
+        /**
+         *  Bitmap池用来允许不同尺寸的Bitmap被重用，
+         *  这可以显著地减少因为图片解码像素数组分配内存而引发的垃圾回收。
+         *  默认情况下Glide使用LruBitmapPool作为Bitmap池，
+         *  LruBitmapPool采用LRU算法保存最近使用的尺寸的Bitmap。
+         *  我们可以通过它的构造器设置最大缓存内存大小：
+         */
+        int customBitmapPoolSize = (int) (1.2 * defaultBitmapPoolSize);
+        builder.setBitmapPool(new LruBitmapPool(customBitmapPoolSize));
+
+        /**
+         * MemoryCache用来把resources 缓存在内存里，以便能马上能拿出来显示。
+         * 默认情况下Glide使用LruResourceCache，我们可以通过它的构造器设置最大缓存内存大小：
+         */
+        int customMemoryCacheSize = (int) (1.2 * defaultMemoryCacheSize);
+        builder.setMemoryCache(new LruResourceCache(customMemoryCacheSize));
+
+
+        /**
+         * 可 设置一个用来存储Resource数据和缩略图的DiskCache实现，不过该设置已过时，不推荐使用
+         */
+        //缓存目录名字
+        String cacheDirectoryName = "glideImage";
+        /**
+         * 设置一个用来创建DiskCache的工厂。
+         * 默认情况下Glide使用InternalCacheDiskCacheFactory内部工厂类创建DiskCache，
+         * 缓存目录为程序内部缓存目录/data/data/your_package_name/image_manager_disk_cache/(不能被其它应用访问)且缓存最大为250MB。
+         * 当然，可以通过InternalCacheDiskCacheFactory构造器更改缓存的目录和最大缓存大小
+         * builder.setDiskCache(new InternalCacheDiskCacheFactory(context,
+         cacheDirectoryName, 1024*1024*30));
+         *还可以指定缓存到外部磁盘SD卡上：
+         *
+         */
+
+        /**
+         * 存放在data/data/xxxx/cache/
+         * 参数一  存放在目录下的名称
+         * 参数二  存放目录大小
+         */
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, cacheDirectoryName, 1024 * 1024 * 30));
+        //存放在外置文件浏览器
+//        builder.setDiskCache(new ExternalCacheDiskCacheFactory(context, "glide", 1024 * 1024 * 30));
+
+//        builder.setDiskCache(
+//                new DiskCache.Factory() {
+//                    @Override
+//                    public DiskCache build() {
+//                        File cacheLocation = getMyCacheLocationBlockingIO();
+//                        cacheLocation.mkdirs();
+//                        return DiskLruCacheWrapper.get(cacheLocation, 1024 * 1024 * 30);
+//                    }
+//                });
+
+    }
+
+    /**
+     * 用来在Glide单例创建之后但请求发起之前注册组件，该方法每次实现只会被调用一次。通常在该方法中注册ModelLoader。
+     */
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+        // register ModelLoaders here.
+        //glide.register(GlideConfiguration.class, InputStream.class,
+        //new GlideUrlLoader(context).Factory());
+    }
+}
+
---
+++ app/src/main/java/com/example/androidlongs/popwindowapplication/img/GlideFunctionUtils.java
@@ -0,0 +1,280 @@
+package com.example.androidlongs.popwindowapplication.img;
+
+import android.content.Context;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.bumptech.glide.DrawableRequestBuilder;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.androidlongs.popwindowapplication.R;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.net.URL;
+
+/**
+ * Created by androidlongs on 16/12/1.
+ * 站在顶峰，看世界
+ * 落在谷底，思人生
+ */
+
+public class GlideFunctionUtils {
+
+    /**
+     * 使用单例模式来调用工具类方法
+     */
+    private GlideFunctionUtils() {
+    }
+
+    private static class SingleGlideFunction {
+        private static GlideFunctionUtils glideFunctionUtils = new GlideFunctionUtils();
+    }
+
+    public static GlideFunctionUtils getInstance() {
+        return SingleGlideFunction.glideFunctionUtils;
+    }
+
+    /**
+     * 初始化操作
+     */
+    private void initFunction() {
+
+    }
+
+
+    /**
+     * 基本加载图片
+     */
+    private void baseLoadImage(Context context, String url, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .into(imageView);
+    }
+
+    /**
+     * 基本加载图片
+     *
+     * @param url    图片地址
+     * @param height 加载图片的高度
+     * @param width  加载图片的宽度
+     */
+    private void baseLoadImage(Context context, String url, int width, int height, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .override(width, height)
+                .into(imageView);
+    }
+    /**
+     * 基本加载图片
+     * 设置缓存模式
+     * @param url    图片地址
+     */
+    private void baseLoadAndCacheImage(Context context, String url, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
+    }
+
+    /**
+     * 加载SD卡目录上的图片
+     */
+    public void loadSdcardFile(Context context, String path, ImageView imageView) {
+        Glide.with(context)
+                .load("file://" + path)
+                .into(imageView);
+    }
+
+    /**
+     * 加载asset目录下的图片
+     */
+    public void loadAssetFile(Context context, String path, ImageView imageView) {
+        Glide.with(context)
+                .load("file:///android_asset/" + path)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 raw目录下的图片
+     */
+    public void loadRawFile(Context context, String path, ImageView imageView) {
+        Glide.with(context)
+                .load("Android.resource://com.frank.glide/raw/" + path)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 raw目录下的图片
+     */
+    public void loadRawFile(Context context, int path, ImageView imageView) {
+        Glide.with(context)
+                .load("Android.resource://com.frank.glide/raw/" + path)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 drawable 目录下的图片
+     *
+     * @param path String 类型路径
+     */
+    public void loadDrawableFile(Context context, String path, ImageView imageView) {
+        Glide.with(context)
+                .load("android.resource://com.frank.glide/drawable/" + path)
+                .into(imageView);
+    }
+
+    /**
+     * 加载drawable目录下图片
+     *
+     * @param path int 类型路径 例如 资源ID
+     */
+    public void loadDrawableFile(Context context, int path, ImageView imageView) {
+        Glide.with(context)
+                .load("android.resource://com.frank.glide/drawable/" + path)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 File 类型文件
+     *
+     * @param file 文件
+     */
+    public void loadFile(Context context, File file, ImageView imageView) {
+        Glide.with(context)
+                .load(file)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 Uri 类型文件
+     *
+     * @param uri 文件
+     */
+    public void loadUriFile(Context context, Uri uri, ImageView imageView) {
+        Glide.with(context)
+                .load(uri)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 URL 类型文件
+     *
+     * @param url 文件
+     */
+    public void loadURLFile(Context context, URL url, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .into(imageView);
+    }
+
+    /**
+     * 加载 Byte数组 类型文件
+     *
+     * @param bytes 文件
+     */
+    public void loadURLFile(Context context, byte[] bytes, ImageView imageView) {
+        Glide.with(context)
+                .load(bytes)
+                .into(imageView);
+    }
+
+
+    /**
+     * 加载图片 并设置加载略缩图
+     * 先显示的 略缩图为原图的 1/10
+     */
+    public void loadImageAndOriginFunction(Context context, String url, ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .thumbnail(0.1f)
+                .into(imageView);
+    }
+
+    /**
+     * 加载图片 并设置加载略缩图
+     * 先显示的 略缩图为指定的 显示的图片
+     */
+    public void loadImageAndDefaultFunction(Context context, String url, ImageView imageView) {
+
+        DrawableRequestBuilder<Integer> builder = Glide
+                .with(context)
+                .load(R.mipmap.defaul_image);
+
+        Glide.with(context)
+                .load(url)
+                .thumbnail(builder)
+                .into(imageView);
+    }
+
+    /**
+     * 加载图片 并设置加载略缩图
+     * 先显示的 略缩图为指定的 显示的图片
+     * 高优先加载顺序
+     */
+    public void loadImageAndHighPriority(Context context, String url, ImageView imageView) {
+
+        DrawableRequestBuilder<Integer> builder = Glide
+                .with(context)
+                .load(R.mipmap.defaul_image);
+
+        Glide.with(context)
+                .load(url)
+                .thumbnail(builder)
+                .priority(Priority.HIGH)
+                .into(imageView);
+    }
+
+
+    /**
+     * 加载图片 并设置加载略缩图
+     * 先显示的 略缩图为指定的 显示的图片
+     * 低优先加载顺序
+     */
+    public void loadImageAndLowPriority(Context context, String url, ImageView imageView) {
+
+        DrawableRequestBuilder<Integer> builder = Glide
+                .with(context)
+                .load(R.mipmap.defaul_image);
+
+        Glide.with(context)
+                .load(url)
+                .thumbnail(builder)
+                .priority(Priority.LOW)
+                .into(imageView);
+    }
+
+
+    /**
+     * 加载图片 结合使用弱引用方式
+     * 默认情况下，Glide会根据with()使用的Activity或Fragment的生命周期自动调整资源请求以及资源回收。
+     * 但是如果有很占内存的Fragment或Activity不销毁而仅仅是隐藏视图，那么这些图片资源就没办法及时回收，即使是GC的时候
+     */
+    public void loadWeakImage(Context context, String url, ImageView imageView) {
+        final WeakReference<ImageView> imageViewWeakReference = new WeakReference<>(imageView);
+        ImageView target = imageViewWeakReference.get();
+        if (target != null) {
+            Glide.with(context)
+                    .load(url)
+                    .into(target);
+        }
+    }
+
+
+    /**
+     * 清除内存缓存
+     */
+    public void clerMemory(Context context) {
+        // 必须在UI线程中调用
+        Glide.get(context).clearMemory();
+    }
+
+    /**
+     * 清除磁盘缓存
+     */
+    public void clerDisCache(Context context) {
+        // 必须在后台线程中调用
+        //Glide.get(context.getApplicationContext()).clearDiskCache();
+    }
+}
